341d7e8362c117a29c6f55fe8ccd716969cc6da8
Mehdi Bichari <mehdi.bichari@mbp-de-admin.home>
2019-12-13 20:15:10 +0100

    Add first move logic to macgyver

---
+++ config.py
@@ -0,0 +1,6 @@
+"""
+File containing all configurations for the game
+"""
+import pygame
+
+MACGYVER_PICTURE = pygame.image.load('ressource/Images/MacGyver.png').convert()
--- macgyver.py
+++ macgyver.py
@@ -1,5 +1,33 @@
-"""
-This class is set to represent our hero, macguyver, his methods and attributes
-"""
+"""This module is set to implement MacGyver inside the maze"""
+import operator
+import config
+
 
 class MacGyver:
+    """This class is set to represent our hero, macguyver, his methods and attributes. """
+    def __init__(self, maze):
+        self.position = (0, 0)
+        self.item_count = 0
+        self.picture = config.MACGYVER_PICTURE
+        self.maze = maze
+
+    def move(self, direction: str):
+        """This methods gives the ability to MacGyver to move inside the maze.
+
+        Args:
+            direction: the direction where MacGyver is trying to move
+        """
+        new_position = (0, 0)
+
+        if direction == 'up':
+            move = (0, 1)
+            new_position = tuple(map(operator.add, self.position, move))
+            if new_position not in self.maze.wall:
+                self.position = new_position
+            # TODO: implement the out of the box
+        elif direction == 'down':
+            pass
+        elif direction == 'left':
+            pass
+        elif direction == 'right':
+            pass
