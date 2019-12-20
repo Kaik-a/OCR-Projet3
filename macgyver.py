97a16a69af92c2bc1404900cec0b77cc22d6eb03
mehdi.bichari <mehdi.bichari@outscale.com>
2019-12-20 14:58:52 +0100

    Added a method to set the new position of MacGyver

--- macgyver.py
+++ macgyver.py
@@ -17,27 +17,23 @@ class MacGyver:
         Args:
             direction: the direction where MacGyver is trying to move
         """
-        new_position = (0, 0)
+
+        def set_position(try_move: tuple):
+            """If try_move is in accepted paths, we define the new position of
+            McaGyver"""
+            new_position = tuple(map(operator.add, self.position, try_move))
+            if new_position in self.maze.path:
+                self.position = new_position
 
         if direction == 'up':
             move = (0, 1)
-            new_position = tuple(map(operator.add, self.position, move))
-            if new_position in self.maze.path:
-                self.position = new_position
+            set_position(move)
         elif direction == 'down':
             move = (0, -1)
-            new_position = tuple(map(operator.add, self.position, move))
-            if new_position in self.maze.path:
-                self.position : new_position
+            set_position(move)
         elif direction == 'left':
             move = (-1, 0)
-            new_position = (tuple(map(operator.add, self.position, move)))
-            if new_position in self.maze.path:
-                self.position = new_position
+            set_position(move)
         elif direction == 'right':
             move = (1, 0)
-            new_position = tuple(map(operator.add, self.position, move))
-            if new_position in self.maze.path:
-                self.position = new_position
-
-    
+            set_position(move)
