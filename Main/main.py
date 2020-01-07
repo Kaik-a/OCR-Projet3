f2df3a79e7395852125da1b080ca7bfe1787090c
mehdi.bichari <mehdi.bichari@outscale.com>
2020-01-07 16:19:48 +0100

    Added pygames specs

--- Main/main.py
+++ Main/main.py
@@ -1,21 +1,51 @@
 """File to launch in order to play MacGyver's maze game"""
 
 from Model.level import Level
+from config import WINDOW_SIZE, MACGYVER_PICTURE, WINDOW_TITLE
+from View.maze_view import create_maze_view
 import pygame
+from pygame.locals import *
 
 
 def main():
     pygame.init()
+    window = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
+
+    icon = pygame.image.load(MACGYVER_PICTURE)
+    pygame.display.set_icon(icon)
+
+    pygame.display.set_caption(WINDOW_TITLE)
 
     level = Level()
 
-    while True:
-        direction = input('Please enter a direction')
-        level.macgyver.move(direction, level.maze)
+    playing = True
 
-        if level.macgyver.position == level.guardian.position:
-            showdown = level.guardian.block_exit(level.macgyver)
-            level.result(showdown)
+    while True:
+        while playing:
+            pygame.time.Clock().tick(30)
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    playing = False
+                    principal_loop = False
+                elif event.type == KEYDOWN:
+                    if event.key == K_RIGHT:
+                        level.macgyver.move('right')
+                    elif event.key == K_LEFT:
+                        level.macgyver.move('left')
+                    elif event.key == K_DOWN:
+                        level.macgyver.move('down')
+                    elif event.key == K_UP:
+                        level.macgyver.move('up')
+
+                create_maze_view(level.maze, window)
+                window.blit(level.macgyver.picture, level.macgyver.position)
+
+                pygame.display.flip()
+
+                if level.macgyver.position == level.guardian.position:
+                    showdown = level.guardian.block_exit(level.macgyver)
+                    level.result(showdown)
 
 
 if __name__ == "__main__":
--- config.py
+++ config.py
@@ -5,5 +5,9 @@ File containing all configurations for the game
 DEFAULT_FILE = 'ressource/Mazes/main'
 GUARDIAN_PICTURE = 'ressource/Images/Gardien.png'
 MACGYVER_PICTURE = 'ressource/Images/MacGyver.png'
-SPRITE_SHEET = 'ressource/Images/floor-tiles-20x20'
+SPRITE_SHEET = 'ressource/Images/floor-tiles-20x20.png'
+SPRITE_SIZE = 20
 RIP_PICTURE = 'ressource/Images/RIP.svg'
+TOTAL_SIZE = 15
+WINDOW_SIZE = SPRITE_SIZE * TOTAL_SIZE
+WINDOW_TITLE = "MacGyver's escape game"
